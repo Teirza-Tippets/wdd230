3efff9f96178f4f2bc44d4b40a87964907011e78
Teirza-Tippets <tgtippets@gmail.com>
2024-05-18 15:37:38 -0600

    git

---
+++ lesson04/script/scripts.js
@@ -0,0 +1,24 @@
+// getdates for footer of home page
+const currentYear = 2024;
+document.querySelector('#year').textContent = currentYear;
+
+// name for footer
+const myName = 'Teirza Tippets';
+document.querySelector('#name').textContent = myName;
+
+// state or country
+const myProject = 'Chamber of Commerce';
+document.querySelector('#project').textContent = myProject;
+
+// last mmodified 
+
+let lastModified = new Date(document.lastModified);
+document.querySelector('#last-modification').textContent = lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
