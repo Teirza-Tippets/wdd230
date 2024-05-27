22d7ef0c846c08586a1a06c11b5c530365eac02f
Teirza-Tippets <tgtippets@gmail.com>
2024-05-27 10:18:41 -0600

    git

--- lesson04/script/scripts.js
+++ lesson04/script/scripts.js
@@ -25,3 +25,52 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+
+//light and dark mode 
+
+const modeButton = document.querySelector('#mode');
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const navi = document.querySelector('nav');
+const footer = document.querySelector('footer');
+const infoBoard = document.querySelector('.info-board');
+const events = document.querySelector('.events');
+const weather = document.querySelector('.weather');
+const memberSpotlights = document.querySelector('.member-spotlights');
+
+modeButton.addEventListener('click', () => {
+	if (modeButton.textContent.includes('🕶️')) {
+		body.style.background = '#000';
+		body.style.color = '#fff';
+		header.style.background = '#23252D';
+		header.style.color = '#fff';
+		navi.style.background = '#23252D';
+		navi.style.color = '#fff';
+		footer.style.background = '#23252D';
+		footer.style.color = '#fff';
+		events.style.background = '#3A3C44';
+		events.style.color = '#fff';
+		weather.style.background = '#3A3C44';
+		weather.style.color = '#fff';
+		memberSpotlights.style.background = '#3A3C44';
+		memberSpotlights.style.color = '#fff';
+		modeButton.textContent = '🔆';
+	} else {
+		body.style.background = '#eee';
+		body.style.color = '#fff';
+		header.style.background = '#284B63';
+		header.style.color = '#fff';
+		navi.style.background = '#284B63';
+		navi.style.color = '#fff';
+		footer.style.background = '#284B63';
+		footer.style.color = '#fff';
+		events.style.background = '#3C6E71';
+		events.style.color = '#fff';
+		weather.style.background = '#3C6E71';
+		weather.style.color = '#fff';
+		memberSpotlights.style.background = '#3C6E71';
+		memberSpotlights.style.color = '#fff';
+		modeButton.textContent = '🕶️';
+	}
+});
