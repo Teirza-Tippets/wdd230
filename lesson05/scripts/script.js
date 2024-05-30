8aae0f55fd2515b57f2ea0679f06ea2706f28c42
Teirza-Tippets <tgtippets@gmail.com>
2024-05-30 17:53:08 -0600

    git

--- lesson05/scripts/script.js
+++ lesson05/scripts/script.js
@@ -1,24 +1,21 @@
-const list = document.querySelector('ul');
-const input = document.querySelector('input');
-const button = document.querySelector('button');
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
 
-button.onclick = function() {
-  let myItem = input.value;
-  input.value = '';
-
-  const listItem = document.createElement('li');
-  const listText = document.createElement('span');
-  const listBtn = document.createElement('button');
-
-  listItem.appendChild(listText);
-  listText.textContent = myItem;
-  listItem.appendChild(listBtn);
-  listBtn.textContent = '❌';
-  list.appendChild(listItem);
-
-  listBtn.onclick = function(e) {
-    list.removeChild(listItem);
-  }
-
-  input.focus();
-}
+button.addEventListener('click', function() {
+    if (input.value != "") {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        li.append(deleteButton);
+        list.append (li);
+        
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        
+        input.focus();
+        input.value = '';}
+});
