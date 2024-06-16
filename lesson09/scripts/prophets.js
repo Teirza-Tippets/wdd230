de2f6dd0f307710758875943c6ff0bc7de9d4f24
Teirza-Tippets <tgtippets@gmail.com>
2024-06-15 21:07:40 -0600

    git

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -16,20 +16,25 @@ async function getProphetData() {
     prophets.forEach((prophet) => {
       // Create elements to add to the div.cards element
       let card = document.createElement('section');
-      let fullName = document.createElement('__'); // fill in the blank
+      let fullName = document.createElement('h2'); 
+      let birthday = document.createElement('h4');
+      let order = document.createElement('p');
       let portrait = document.createElement('img');
   
       // Build the h2 content out to show the prophet's full name
-      fullName.textContent = `${prophet._____} ____________`; // fill in the blank
+      fullName.textContent = ` ${prophet.name} ${prophet.lastname} `;
+      birthday.textContent = `Birthdate: ${prophet.birthdate}`;
+      order.textContent = `Order: ${prophet.order}`;
       // Build the image portrait by setting all the relevant attributes
       portrait.setAttribute('src', prophet.imageurl);
-      portrait.setAttribute('alt', `Portrait of ${prophet.____} ______________`); // fill in the blank
+      portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}Latter-Day Prophet`); // fill in the blank
       portrait.setAttribute('loading', 'lazy');
       portrait.setAttribute('width', '340');
       portrait.setAttribute('height', '440');
-  
       // Append the section(card) with the created elements
-      card.appendChild(_______); //fill in the blank
+      card.appendChild(fullName); 
+      card.appendChild(birthday);
+      card.appendChild(order);
       card.appendChild(portrait);
   
       cards.appendChild(card);
