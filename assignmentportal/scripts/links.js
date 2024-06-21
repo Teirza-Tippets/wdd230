9bcbe24e4b55baf366e41eaebdd0c5e7ec4cd0a9
Teirza-Tippets <tgtippets@gmail.com>
2024-06-20 19:18:40 -0600

    git

--- assignmentportal/scripts/links.js
+++ assignmentportal/scripts/links.js
@@ -1,36 +1,35 @@
-const list = document.querySelector('.activities');
+const listEl = document.querySelector('#weeks');
 
 const baseURL = "https://teirza-tippets.github.io/wdd230/";
 const linksURL = "https://teirza-tippets.github.io/wdd230/data/links.json";
 
 
-
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    // console.log(data);
-    displayLinks (data);
-  }
-  
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  //console.log(data);
+  let dataList = data['lessons'];
+  dataList.forEach(displayLinks);
+}
 
-function displayLinks(data){
-    //connect to the json file to the elements in the html
-    let lesson = document.createElement('li');
-    //number correlation to the activity
-    lesson.textContent = `${data.lesson}: `;
-
-    data.links.forEach(link => {
-    //connect to the url and titles of each activity
-      let a = document.createElement('a');
-
-
-    //attributes for links
-      a.setAttribute('href', link.url);
-      a.textContent = `${link.title}| `;
-      lesson.appendChild(a);
-    });
-
-    list.appendChild(lesson);
-  };
+function displayLinks(data) {
+      //Build Element
+      let li = document.createElement("li");
+      //Build li
+      li.textContent = `${data.lesson}: `;
+     
+      //Loop the link info so we get them all
+      data.links.forEach(link => {
+          //Build Element
+          let a = document.createElement("a");
+          //Set attributes for a element
+          a.setAttribute("href", link.url);
+          a.textContent = `${link.title}| `;
+          li.appendChild(a);    
+          });
+ 
+      //Loop back to create a new li
+      listEl.appendChild(li);
+      };
 
 getLinks();
