67f8a8142385e5f1e4b11ae42ddbc5ffe4f989e3
Teirza-Tippets <tgtippets@gmail.com>
2024-06-25 17:32:30 -0600

    git

--- chamber/script/windchill.js
+++ chamber/script/windchill.js
@@ -1,4 +1,6 @@
 // calculation of windchill
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const temperatureElement = document.getElementById("temperature");
     const windspeedElement = document.getElementById("windspeed");
@@ -6,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const temperature = parseFloat(temperatureElement.textContent);
     const windspeed = parseFloat(windspeedElement.textContent);
-
+z
     function calculateWindChill(temp, speed) {
         // Wind chill calculation formula
         return 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
