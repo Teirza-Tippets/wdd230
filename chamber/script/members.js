319a04f664951fc014cca337649c667dbdbe43d7
Teirza-Tippets <tgtippets@gmail.com>
2024-07-05 13:45:34 -0600

    git

--- chamber/script/members.js
+++ chamber/script/members.js
@@ -1,5 +1,10 @@
 const card = document.querySelector('#cards');
+const toggleButton = document.querySelector('#toggleView');
 const memberURL = 'https://teirza-tippets.github.io/wdd230/chamber/data/members.json';
+
+toggleButton.addEventListener('click', () => {
+    card.classList.toggle('list-view');
+});
 
 async function getMem() {
     const response = await fetch(memberURL);
@@ -7,12 +12,13 @@ async function getMem() {
     console.log(data);
     let dataMem = data['members'];
     dataMem.forEach(displayMem);
-  }
+}
 
 function displayMem(data) {
-    //section elements list
+    // section elements list
     let section = document.createElement('section');
     let logo = document.createElement('img');
+    let details = document.createElement('div');
     let name = document.createElement('h2');
     let url = document.createElement('a');
     let address = document.createElement('p');
@@ -20,38 +26,42 @@ function displayMem(data) {
     let email = document.createElement('p');
     let memLevel = document.createElement('h3');
 
-    //img attributes
+    // img attributes
     logo.setAttribute('src', data.logo);
     logo.setAttribute('alt', 'logo');
     logo.setAttribute('loading', 'lazy');
     logo.setAttribute('width', '150');
 
-    //h2 elements
+    // details div class
+    details.classList.add('details');
+
+    // h2 elements
     name.textContent = `${data.name}`;
 
-    //a element for webiste
+    // a element for website
     url.setAttribute('href', data.url);
     url.textContent = `${data.url}`;
 
-    //p for address & phone #
+    // p for address & phone #
     address.textContent = `${data.address}`;
     phone.textContent = `${data.phone}`;
     email.textContent = `${data.email}`;
 
-    //h3 elements
+    // h3 elements
     memLevel.textContent = `${data['membership-level']}`;
 
-    //appending loop
+    // appending loop
+    details.appendChild(name);
+    details.appendChild(url);
+    details.appendChild(address);
+    details.appendChild(phone);
+    details.appendChild(email);
+    details.appendChild(memLevel);
+
     section.appendChild(logo);
-    section.appendChild(name);
-    section.appendChild(url);
-    section.appendChild(address);
-    section.appendChild(phone);
-    section.appendChild(email);
-    section.appendChild(memLevel);
+    section.appendChild(details);
 
     card.appendChild(section);
-
 }
 
 getMem();
