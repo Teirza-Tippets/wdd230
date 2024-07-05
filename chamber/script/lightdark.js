0fbf54ddb3284be69c6e0e9e71c5c2ee26ac9ee9
Teirza-Tippets <tgtippets@gmail.com>
2024-07-05 10:42:25 -0600

    git

--- chamber/script/lightdark.js
+++ chamber/script/lightdark.js
@@ -24,28 +24,13 @@ modeButton.addEventListener('click', () => {
 	} else {
         body.style.background = '#FFFFFF';
         body.style.color = '#FFFFFF';
-        header.style.background = '#284B63';
+        header.style.background = '#586F7C';
         header.style.color = '#FFFFFF';
         nav.style.background = '#284B63';
         nav.style.color = '#FFFFFF';
         main.style.background = '#FFFFFF';
         main.style.color = '#353535';
-        footer.style.background = '#284B63';
+        footer.style.background = '#586F7C';
 		modeButton.textContent = '🕶️';
 	}
 });
-
-
-//#003d5c
-
-// --primary-color: #FFFFFF ;
-// --secondary-color: #284B63 ;
-// --accent1-color: #353535;
-// --accent2-color: #3C6E71;
-// --accent3-color: #D9D9D9;
-
-// #02111B
-// #3F4045
-// #30292F
-// #5D737E
-// #FFFFFF
