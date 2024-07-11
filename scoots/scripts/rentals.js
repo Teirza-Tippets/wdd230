f44658ec9ea58fc0973c275db952b7030cfd2ed6
Teirza-Tippets <tgtippets@gmail.com>
2024-07-10 19:39:08 -0600

    git

---
+++ scoots/scripts/rentals.js
@@ -0,0 +1,52 @@
+const rentalURL = 'https://teirza-tippets.github.io/wdd230/scoots/data/rentals.json';
+const cards = document.querySelector('#cards');
+
+async function getRentals() {
+    const response = await fetch(rentalURL);
+    const data = await response.json();
+    console.log(data);
+    let dataRent = data['rentals'];
+    dataRent.forEach(displayRent);
+  }
+
+function displayRent(data) {
+    //section elements list
+    let div = document.createElement('div');
+    let image = document.createElement('img');
+    let type = document.createElement('h2');
+    let reserve = document.createElement('h4');
+    let walkIn = document.createElement('h4');
+    let numPersons = document.createElement('p');
+
+
+    //img attributes
+    image.setAttribute('src', data.img);
+    image.setAttribute('alt', 'logo');
+    image.setAttribute('loading', 'lazy');
+    image.setAttribute('width', '150');
+
+    //h2 elements
+    type.textContent = `${data.type}`;
+
+
+    //p for address & phone #
+    numPersons.textContent = `${data.numPersons}`;
+   
+
+    //h4 elements
+    reserve.textContent = `Reservations:  Half Day - ${data.reservation['halfDay']} - Full Day - ${data.reservation['fullDay']} `;
+    walkIn.textContent = `Walk-Ins:  Half Day - ${data.walkIN['halfDay']} - Full Day - ${data.walkIN['fullDay']} `;
+
+
+    //appending loop
+    div.appendChild(image);
+    div.appendChild(type);
+    div.appendChild(numPersons);
+    div.appendChild(reserve);
+    div.appendChild(walkIn);
+
+    cards.appendChild(div);
+
+}
+
+getRentals();
